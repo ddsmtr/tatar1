63177cce4859644377a63c3d331ff9fb95bb9b60
Oleg <ddosmotr@gmail.com>
2017-10-08 03:58:43 +0300

    almoust finish

--- script.js
+++ script.js
@@ -1,213 +1,228 @@
 (function(){
-	function getFormData(){
-		const form = document.forms.data;
-		const fx = form.fx.value.replace(/[\s*]/g,"");//звездочки убраны
-		const a = Number(form.left_border.value);
-		const b = Number(form.right_border.value); 
-		const e = 0.1;//Number(form.error.value);
-		//проверить корректность данных. В случае неверного заполнения дать понять пользователю информатиынфм окном
-
-		let arr,arg1,arg2,arg3,sign1,sign2;
-		let count_x = fx.match(/x/ig);
-
-		if(count_x === null){
-			console.log("Некорректная функция");
-		}
-
-		if(count_x.length == 2){ // a и b есть точно
-			arr = fx.split(/[+-]/g);
-			sign1 = fx[fx.indexOf("x")+3];
-			if(arr.length == 3){ // значит все элементы на месте
-				sign2 = fx[fx.indexOf("x",5)+1];
-				for(let tmp,i = 0; i < arr.length; i++){
-					tmp = arr[i].indexOf("x");
-					if(tmp == -1) {
-						arr[i] = Number(arr[i]);
-						continue;
-					}
-					if(tmp == 0){
-						arr[i] = 1;
-					}else{
-						arr[i] = Number(arr[i].substr(0,tmp));
-					}	
-				}
-				arg1 = arr[0];
-				arg2 = arr[1];
-				arg3 = arr[2];
-
-				let f_a = calcFx(a,arg1,arg2,arg3,sign1,sign2);
-		 		let f_b = calcFx(b,arg1,arg2,arg3,sign1,sign2);
-		 		if(a*b < 0){
-					//console.log("все нормально. знак поменялся");
-					document.getElementById("error").innerHTML = "";
-					let x = dichotomy(arg1,arg2,sign1,a,b,e);
-					document.getElementById("dih_n1").innerHTML = x.N;
-					document.getElementById("dih_a1").innerHTML = x.a;
-					//console.log(x);
-					//GOLD SECTION
-					let x2 = goldSection(arg1,arg2,arg3,sign1,sign2,a,b,e);
-					console.log(x2);
-
-					//-----------
-					//let x3 = search(arg1,arg2,arg3,sign1,sign2,a,b,e);
-					console.log("search = "+ x3);
-
-				}else{
-					document.getElementById("error").innerHTML = "<br>Знак не поменялся, выберите другой интервал";
-				}
-			}else{ // нету цешки
-				delete sign2;
-				for(let tmp,i = 0; i < arr.length; i++){
-					tmp = arr[i].indexOf("x");
-					if(tmp == 0){
-						arr[i] = 1;
-					}else{
-						arr[i] = Number(arr[i].substr(0,tmp));
-					}
-				}
-				arg1 = arr[0];
-				arg2 = arr[1];
-				let f_a = calcFx(a,arg1,arg2,sign1);
-		 		let f_b = calcFx(b,arg1,arg2.sign1);
-		 		if(a*b < 0){
-					//console.log("все нормально. знак поменялся");
-					document.getElementById("error").innerHTML = "";
-					let x = dichotomy(arg1,arg2,sign1,a,b,e);
-					//console.log(x);
-					let x2 = goldSection(arg1,arg2,sign1,a,b,e);
-
-				}else{
-					//console.log("Знак не менялся");
-					document.getElementById("error").innerHTML = "<br>Знак не поменялся, выберите другой интервал";
-				}
-
-			}
-		}else{ // один икс в квадрате(первая проверка отмела вариант с несуществованием)
-			console.log("c = "+ 0);
-		}
-		
-	}
-
 	goform.addEventListener("click", getFormData);
-
 })();
-function calcFx(par,arg1,arg2,arg3,sign1,sign2){ // Надо разобраться. либо сделать явное присваивание своим переменным в пробежке по аргументам. но такая хуйня не катит
-	if(arguments.length == 6){
-		if(sign1 == '+'){
-			if(sign2 == '+'){
-				return arg1*Math.pow(par,2)+arg2*par+arg3;
-			}else{
-				return arg1*Math.pow(par,2)+arg2*par-arg3;
+function getFormData(){
+	const form = document.forms.data;
+	const fx = form.fx.value.replace(/[\s*]/g,"");
+	const a = Number(form.left_border.value);
+	const b = Number(form.right_border.value); 
+	const e = [0.1, 0.01, 0.001];
+
+	let regFx = /[^\dx\*\-\+\^.]/ig;
+	try{
+		document.getElementById("error").innerHTML = "";
+		if(fx.match(regFx) != null){
+			throw new Error("нельзя вводить символы типа : " + fx.match(regFx));
+		}
+		if(isNaN(a) || isNaN(b)){
+		 	throw new Error("некорректный интервал. Допустим только ввод чисел");	
+		}
+
+		//весь код
+		if(fx.match(/x/ig).length == 2){// значит есть a и b
+			if(~(fx.indexOf("^"))){// значит степень есть, работаем. Внимение, неочевидный вызов. тильда. означает что если результат отличен от -1
+				const arrArg = fx.split(/[+-]/g);
+				let fxSign = true;
+				if(arrArg[0] == ""){
+					arrArg.splice(0,1);
+					fxSign = false;
+					console.log("Отрицательная функция");
+				}
+				//--------------
+				const sign1 = fx[fx.indexOf("x")+3];
+				let sign2;
+				arrArg.forEach(function(value,i){
+					if(~(value.indexOf("x"))){
+						if(value[0] == "x" && fxSign == true){
+							arrArg.splice(i,1,1);
+							//value = 1;
+							fxSign = true;// чтобы сбросить будевый тип, иначе он будет ко всем аргументам минус лепить
+						}else if(value[0] == "x" && fxSign == false){
+							arrArg.splice(i,1,-1);
+							//value = -1;
+							fxSign = true;
+						}
+						else if(fxSign == false){
+							arrArg.splice(i,1,Number("-" + value.substr(0,value.indexOf("x"))));
+							//value = Number("-" + value.substr(0,value.indexOf("x")));
+							fxSign = true;
+						}	
+						else{
+							arrArg.splice(i,1,Number(value.substr(0,value.indexOf("x"))));
+							//value = Number(value.substr(0,value.indexOf("x")));
+							fxSign = true;
+						}		
+					}
+					else{
+						arrArg.splice(i,1,Number(value));
+						//value = Number(value)
+						fxSign = true;
+					}
+				});
+				if(arrArg.length ==2){ // чисто последний шаг, дополнение аргументов до одинакового количесва у всех выражений
+					sign2 = "+";
+					arrArg.push(0);
+				}else{
+					sign2 = fx[fx.indexOf("x",fx.indexOf("x")+1)+1];
+				}
+				 console.log(arrArg);
+				 console.log("sign1 = "+sign1+" , sign2 = "+sign2);
+
+				// working...
+				//let resFxA = calcFx(a,arrArg[0],arrArg[1],arrArg[2],sign1,sign2);
+				//let resFxB = calcFx(b,arrArg[0],arrArg[1],arrArg[2],sign1,sign2);
+				//console.log("check sign: fxa"+ resFxA+" , fxb = "+ resFxB);
+				//if(resFxA*resFxB < 0){
+					e.forEach(function(value,i){
+						i+=1;
+						let dih_n = "dih_n"+i;
+						let dih_a = "dih_a"+i;
+						let gold_n = "gold_n"+i;
+						let gold_a = "gold_a"+i;
+						let search_n = "search_n"+i;
+						let search_a = "search_a"+i;
+
+						let resultDichotomy = dichotomy(arrArg[0],arrArg[1],sign1,a,b,value);
+						console.log(resultDichotomy);
+						document.getElementById(dih_n).innerHTML = resultDichotomy.N;
+						document.getElementById(dih_a).innerHTML = resultDichotomy.a.toFixed(4);
+						console.log("-----------------------");
+						let resultGold = goldSection(a,b,value,arrArg[0],arrArg[1],arrArg[2],sign1,sign2);
+						console.log(resultGold);
+						document.getElementById(gold_n).innerHTML = resultGold.N;
+						document.getElementById(gold_a).innerHTML = resultGold.a.toFixed(4);
+
+						console.log("-----------------------");
+						let resultSearch = search(a,b,value,arrArg[0],arrArg[1],arrArg[2],sign1,sign2);
+						console.log(resultSearch);
+						document.getElementById(search_n).innerHTML = resultSearch.N;
+						document.getElementById(search_a).innerHTML = resultSearch.a.toFixed(4);
+
+					});
+				//}else{
+				//	console.log("знак не поменялся. необходимо выбрать другой интервал");
+				//}
+
 			}
-		}else{
-			if(sign2 == '+'){
-				return arg1*Math.pow(par,2)-arg2*par+arg3;
-			}else{
-				return arg1*Math.pow(par,2)-arg2*par-arg3;
+			else{	// нет степени - нет работы
+				throw new Error("нет степенной функции. Определить нельзя");
 			}
 		}
-	}
-	else if(arguments.length == 4){	// else if каряво в данном случе, где ELSE?
-		if(sign1 == '+'){
-			return arg1*Math.pow(par,2)+arg2*par;
-		}else{
-			return arg1*Math.pow(par,2)-arg2*par;
+		else{ // есть a или b
+			let message = (~(fx.indexOf("^"))) ? "c = 0" : "нет степенной функции. Определить нельзя";
+			throw new Error(message);
 		}
+		//
 	}
-	
+	catch(e){
+		document.getElementById("error").innerHTML = "Произошла ошибка заполнения данных : " + e.message;
+	}
 };
-
+function calcFx(param,...args){
+	//console.log(args);// выведет только последующие введенные аргументы после 3х
+	if(args.length == 5){ // (arg1,arg2,arg3,sign1,sign2)
+		if(args[3] == '+'){
+			if(args[4] == '+'){
+				return args[0]*Math.pow(param,2)+args[1]*param+args[2];
+			}else{
+				return args[0]*Math.pow(param,2)+args[1]*param-args[2];
+			}
+		}else{
+			if(args[4] == '+'){
+				return args[0]*Math.pow(param,2)-args[1]*param+args[2];
+			}else{
+				return args[0]*Math.pow(param,2)-args[1]*param-args[2];
+			}
+		}
+	}
+	else{
+		console.log("ошибочка");
+	}
+};
 function dichotomy(arg1,arg2,sign1,a,b,e){
-	this.a = a;
-	this.b = b;
-	let dif = Math.abs(b-a);
-	let c;
-	let tmp;
-	let arr;
+	let intervalLengthStart = Math.abs(b-a);
+	let c, valueDxFx;
 	let i = 0;
-
-	let dichotomy = document.getElementById("dichotomy");
-	let result = document.createElement("p");
-	dichotomy.appendChild(result);
-	// dx_f = 2*arg1 sign1 arg2
-	while((b-a) >= e && tmp != 0){
+	while((b-a) >= e && valueDxFx != 0){
 		c = (a+b)/2;
 		if(sign1 == '+'){
-			tmp = (2*arg1)*c + arg2;
-			if(tmp > 0) b = c;
+			valueDxFx = (2*arg1)*c + arg2;
+			if(valueDxFx > 0) b = c;
 			else a = c;
 		}else{
-			tmp = (2*arg1)*c - arg2;
-			if(tmp > 0) b = c;
+			valueDxFx = (2*arg1)*c - arg2;
+			if(valueDxFx > 0) b = c;
 			else a = c;
 		}
-		result.innerHTML = "["+a+";"+b+"] c = "+ c + "<br>";
 		//console.log("["+a+";"+b+"]  c = "+ c);
 		i++;
 	}
-	 result.innerHTML += "<br> x* = "+c;
-	 let alpha = (Math.abs(b-a))/dif; // а N это и есть i. хз.
-	 console.log(alpha);
-	 return {"c":c,
-	 		 "N":i,
-	 		 "a": alpha,
-	 		};//можно будет вернуть объект из полей середины, и оценок
+	return {"x" : c,
+			"a" : (Math.abs(b-a))/intervalLengthStart,
+			"N" : i
+			};
 };
-
-// не работает с x^2 -10x!!!!!!!!!!!!!!!!
-function goldSection(arg1,arg2,arg3,sign1,sign2,a,b,e){
-	this.a = a;
-	this.b = b;
-	//1)
+function goldSection(a,b,e,...args){//	(arg1,arg2,arg3,sign1,sign2)
+	let intervalLengthStart = Math.abs(b-a);
 	let lambda = a + 0.382 * (b - a);
 	let tau = a + 0.618 * (b - a);
-	console.log("lambda = " + lambda + " tau = "+ tau);
-
-	let f_lambda; 
-	let f_tau; 
-	//console.log("f_lambda = " + f_lambda + " f_tau = "+ f_tau);
+	let valueFxFromLambda, valueFxFromTau;
 
 	let i = 0;
-	while((b-a) >=e){
-		f_lambda = calcFx(lambda,arg1,arg2,arg3,sign1,sign2);
-		f_tau = calcFx(tau,arg1,arg2,arg3,sign1,sign2);
-		if(f_lambda > f_tau){
+
+	while((b-a) >= e){
+		if(args.length == 5){
+			valueFxFromLambda = calcFx(lambda, args[0],args[1],args[2],args[3],args[4]);
+			valueFxFromTau = calcFx(tau, args[0],args[1],args[2],args[3],args[4]);
+		}
+		else{
+			console.log("Ошибка");
+		}
+		
+		if(valueFxFromLambda > valueFxFromTau){
 			a = lambda;
 			lambda = tau;
 			tau = a+0.618*(b-a);
-			f_tau = calcFx(tau,arg1,arg2,arg3,sign1,sign2);
+			valueFxFromTau = calcFx(tau, args[0],args[1],args[2],args[3],args[4]);
 		}else{
 			b = tau;
 			tau = lambda;
 			lambda = a + 0.382 * (b - a);
-			f_lambda = calcFx(lambda,arg1,arg2,arg3,sign1,sign2);
+			valueFxFromLambda = calcFx(lambda, args[0],args[1],args[2],args[3],args[4]);
 		}
+		//console.log("["+a+";"+b+"]  c = " + (a+b)/2);
 		i++;
 	}
-	let x = (a+b)/2;
-	return x;
+	return {"x" : (a+b)/2, 
+			"a" : (Math.abs(b-a))/intervalLengthStart,
+			"N" : i
+			};
 };
-function search(arg1,arg2,arg3,sign1,sign2,a,b,e){
-	this.a = a;
-	this.b = b;
+function search(a,b,e,...args){
+	let intervalLengthStart = Math.abs(b-a);
 	let n = (b-a)/e - 1;
-	//console.log("n = "+n);
 	let arr_x = [];
 	for(let i = 0; i < n;i++){
 		arr_x.push((a+i*(b-a)/(n+1)).toFixed(1));
-		console.log("arr["+i+"] = "+ arr_x[i]);
+		//console.log("arr["+i+"] = "+ arr_x[i]);
 	}
-	console.log("arr search length = " + arr_x.length);
-	let min = calcFx(arr_x[0],arg1,arg2,arg3,sign1,sign2);
+	//console.log("arr search length = " + arr_x.length);
+	let min = calcFx(arr_x[0],args[0],args[1],args[2],args[3],args[4]);
 	let x;
 	let tmp;
+	let count = 0;
+
 	for(i in arr_x){
-		tmp = calcFx(arr_x[i],arg1,arg2,arg3,sign1,sign2);
+		tmp = calcFx(arr_x[i],args[0],args[1],args[2],args[3],args[4]);
 		if(tmp < min){
 			min = tmp;
 			x = arr_x[i];
 		}
 		else continue;
+		count++;
 	}
-	return x;
+	return {"x" : Number(x),
+			"a" : ((Math.abs(b-a))/n)/intervalLengthStart, // хз. УТОЧНИТЬ!
+			"N" : Number(count)
+			};
 };
